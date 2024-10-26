71e9c331c7d4188d8906dbe32288fb4d8a7969b2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-26 19:25:23 +0530

    index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 

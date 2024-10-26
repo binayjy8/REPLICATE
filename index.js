13c7253c9ea282fe3f98fef018bccd949aa08919
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-26 19:22:56 +0530

    index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(port, ()=> {
     console.log("listening to the port: 8080");

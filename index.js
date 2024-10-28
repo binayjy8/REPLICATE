ed56f75d66b46700c76359df20fb2d7bdd7b6605
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-28 19:38:26 +0530

    index

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
     posts.push({ username, content});
-    res.redirect("index.ejs");
+    res.redirect("/posts");
 });
 
 app.listen(port, ()=> {

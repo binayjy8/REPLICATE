42337c4f9d883772fccb57eafe6e5fffd40d38c4
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-29 20:00:07 +0530

    index

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id: "1",
+        id: "1a",
         username: "malka",
         content: "This is malkangiri"
     },

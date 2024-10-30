e170c4c112d3d0be4c207c731810dee1114bf264
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-30 21:44:48 +0530

    index

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post("/posts", (req, res) => {
 
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
-    res.send("hap");
+    res.send("happ");
 });
 
 app.listen(port, ()=> {

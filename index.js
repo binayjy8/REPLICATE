c19ebffc951c2438fd6b12a1487891dca713c845
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-11-01 19:57:06 +0530

    index

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     res.send("happy deewali enjoy this");
     res.send("don't give the excuse");
-    res.send("cherry fi");
+    res.send("cherry fin");
 });
 
 app.listen(port, ()=> {

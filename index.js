3651b6228b21a6b5d0eae4eeb1222e099608fe69
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-11-12 21:49:15 +0530

    show page

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/posts/:id", (req, res) => {
 
 app.patch("/posts/:id", (req, res) => {
     let { id } = req.params;
-    res.send("give up the lazyness other wis");
+    res.send("give up the lazyness other wise");
 });
 
 app.listen(port, ()=> {

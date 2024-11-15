167c1f2494a043ef93019f945dce4c8f1fbc6ca0
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-11-15 20:02:59 +0530

    show page

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.patch("/posts/:id", (req, res) => {
     let newContent = req.body.content;
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
-    res.send("post request working nd thsi");
+    res.send("post request working nd thsi t");
 });
 
 app.get("/posts/:id/edit", (req, res) => {

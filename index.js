32615c22411738e79c08dd70e8d7f6c596a6c267
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-11-19 23:12:05 +0530

    show page

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", { post });
 });
 
-app.get("/p");
+app.get("/po");
 
 app.listen(port, ()=> {
     console.log("listening to the port: 8080");
